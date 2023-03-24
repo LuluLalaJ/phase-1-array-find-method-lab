6622e7cc8625f3001e22d99dc2b82403ad925850
Chen Jiang <chen.jiang.1206@gmail.com>
2023-03-24 12:07:28 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function superbowlWin(array) {
+    if (array.find(testingObject) !== undefined) {
+        return array.find(testingObject).year
+    } else {
+        return array.find(testingObject)
+    }
+    
+}
+
+//helper function 
+function testingObject(element, index, array) {
+    return element.result === "W" 
+}
+
